eb279ca4039e38aeb707d1d33aa88fff3ecdee45
nemo <nemo@protocol.ai>
2021-04-28 08:22:34 -0400

    fix: don't use asm for aarch64 on apple

--- sha2/build.rs
+++ sha2/build.rs
@@ -1,12 +1,13 @@
 fn main() {
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+    let target_vendor = std::env::var("CARGO_CFG_TARGET_VENDOR").unwrap_or_default();
 
     let mut build256 = cc::Build::new();
     let (sha256_path, sha512_path) = if target_arch == "x86" {
         ("src/sha256_x86.S", "src/sha512_x86.S")
     } else if target_arch == "x86_64" {
         ("src/sha256_x64.S", "src/sha512_x64.S")
-    } else if target_arch == "aarch64" {
+    } else if target_arch == "aarch64" && target_vendor != "apple" {
         build256.flag("-march=armv8-a+crypto");
         ("src/sha256_aarch64.S", "")
     } else {
